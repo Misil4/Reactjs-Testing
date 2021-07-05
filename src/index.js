8157d4038a57d6a8cdcd95bb5ff022b143d18bd8
Silva Marques <csilvamarq@tecnun.es>
2021-07-05 10:44:37 +0200

    Version 0.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+const  user = {
+  date: new Date(),
+  text: 'Mejores pilotos de la f1',
+  author: {
+    names: ['button','alonso','barrichelo'],
+    ages: [43,39,50]
+  }
+}
+class MESSAGE extends React.Component {
+  constructor(props) {
+  super(props);
+  }
+  render() {
+    return (<>
+      <div className="Box">{<Clock/>} tiene usted {user.author.ages[1]} años
+      </div><div className="Nav"><Nav /><button className="register">Register</button></div></>
+    )
+  }
+}
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <li>
+        <ul>Inicio</ul>
+        <ul>Historia</ul>
+        <ul>Tienda</ul>
+        <ul>Ver mas</ul>
+        </li>
+    )
+  }
+}
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {date: new Date()};
+    }
 
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000);
+    }
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  render() {
+    return (
+      <div>
+        <h1>Buenos dias, {user.author.names[1]}</h1>
+        <h2>Son las {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MESSAGE />,
   document.getElementById('root')
 );
 

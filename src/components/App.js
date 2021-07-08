919ec0e06068280774250b83f14f50525c8d0e2b
Misil <carlossilvamarques.cs@gmail.com>
2021-07-08 12:51:29 +0200

    Version 0.2

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class MESSAGE extends React.Component {
           <main>
           <>
       <div className="Box">{<Clock/>} tiene usted {user.author.ages[1]} años
-      </div><div className="Nav"><Nav /><Link to="/register">Register</Link></div></>
+      </div><div className="Nav"><Nav /><Link to="/register">Register</Link><Text /></div></>
             {this.props.children}
           </main>
     
@@ -45,7 +45,7 @@ class Nav extends React.Component {
   }
   render() {
     return (
-       <ul>{this.state.nav.map((menu) =><li key={menu}> {menu} </li>)}</ul>
+       <ul>{this.state.nav.map((menu) =><li key={menu}><Link to={menu}>{menu}</Link></li>)}</ul>
     )
   }
 }
@@ -67,6 +67,15 @@ class Historia extends React.Component {
         </ul>
       </div>
     )
+    }
+  }
+  class Title extends React.Component {
+    render() {
+      return (
+        <div className="App-logo">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" width="200rem" height="200rem" alt=""></img>
+        </div>
+      );
     }
   }
 class Clock extends React.Component {
@@ -97,13 +106,28 @@ class Clock extends React.Component {
     );
   }
 }
-// You can choose your kind of history here (e.g. browserHistory)
-// Your routes.js file
+class Text extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          text: "Occaecat sint veniam fugiat irure exercitation quis officia cillum eiusmod aliqua exercitation nostrud aute deserunt. Officia est do cillum et amet adipisicing esse pariatur qui veniam tempor. Eiusmod consequat nisi do qui duis commodo proident pariatur proident ipsum sit."
+        }
+      }
+      render() {
+        return (
+          <div className="text">
+            <h1>Bienvenido a nuestra nueva pagina web</h1><br />
+            <p>{this.state.text}</p><br />
+            <img src="https://www.lavanguardia.com/files/content_image_mobile_filter/uploads/2018/06/22/5fa4335996cdf.jpeg" height="auto" width="auto" id="img" alt="Hola"></img>
+          </div>
+        );
+      }
+}
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -113,17 +137,23 @@ export default function App() {
             </li>
             <li><Link to="/register">Register</Link></li>
           </ul>
-        </nav>
+        </nav> */}
 
         <Switch>
-          <Route path="/clock">
-            <Historia/>
+          <Route path="/Inicio">
+            <MESSAGE/>
+          </Route>
+          <Route path="/Historia">
+            <Title />
+          </Route>
+          <Route path="/Tienda">
+            <Historia />
+          </Route>
+          <Route path="/Ver mas">
+            <Nav />
           </Route>
           <Route path="/">
             <MESSAGE />
-          </Route>
-          <Route path="/register">
-            <Historia />
           </Route>
         </Switch>
       </div>
